2881fab47ad9e8ed0faa397b57d1322e50353c61
Andrew <8911102@gmail.com>
2020-06-27 19:16:17 +0500

    problem with Content Security Policy was fixed

--- source/js/sendForm.js
+++ source/js/sendForm.js
@@ -2,19 +2,21 @@
 
 (function () {
   var TIMEOUT_SEND_FORM = 3000; // ms
-  var URLtoSendForm = 'https://api.github.com/search/repositories';
+  // var URLtoSendForm = 'https://api.github.com/search/repositories/';
+  var URLtoSendForm = 'https://api.github.com/search/code?user:mozilla&page=14';
   var form = document.querySelector('.ad-form');
 
   function submitForm(evt) {
     var formData = new FormData(form);
     var xhr = new XMLHttpRequest();
+    var inputValue = document.querySelector("inputField").value;
 
     xhr.open('GET', URLtoSendForm, true);
 
     xhr.addEventListener('load', function () {
 
       if (xhr.status === 200 && xhr.readyState === 4) {
-        window.successSendForm.successSendForm();
+        console.log('success send form')
       } else {
         window.errorSendForm.onError();
       }
